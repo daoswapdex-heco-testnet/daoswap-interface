4bc55d2384250d2769b8d1d4e3a80631e4af64ea
daoswap002 <daoswap@daoswap.cc>
2021-07-05 12:24:12 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d78aed56925272bdc173c592e5f031a1.js"
+  "./precache-manifest.8e5eb80d1501f730a71a73ff16b8d132.js"
 );
 
 self.addEventListener('message', (event) => {
